25ce21f27821d1d683d53dba1d2ede079ee7d3f7
Nicholas Leavitt <nicholas.leavitt@indexexchange.com>
2016-09-01 09:51:31 -0400

    Added mysql to package.json, added DBM init to index.

--- index.ts
+++ index.ts
@@ -3,6 +3,10 @@
 import * as express from 'express';
 import * as http from 'http';
 
+import { DatabaseManager } from './lib/database-manager';
+
 let startServer: Function = require('./server');
+
+DatabaseManager.initialize();
 
 startServer();
