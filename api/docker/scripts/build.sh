c1f8611647bbaa21cf20602b0d7639cc9e845972
Eusebio Olalde <eusebio.olalde@indexexchange.com>
2016-09-02 16:44:11 -0400

    ATW76_api-core_scripts and docker files

--- api/docker/scripts/build.sh
+++ api/docker/scripts/build.sh
@@ -8,10 +8,13 @@ SERVICE_NAME=${NAME-"api"}
 GIT_COMMIT=$(git rev-parse --short HEAD)
 DATE=`date +%Y-%m-%d`
 
-#this defines the build context directory, CHANGE THIS TO YOUR LOCAL src LOCATION to use locally
-#BUILD_CONTEXT=/home/eusebioolalde/work/workLocal <-- e.g.
-BUILD_CONTEXT=/src
+#this defines the build context directory,
+#BUILD_CONTEXT=/src
 
+# Test for any BUILD_CONTEXT built from this script's location [ SEEMS TO WORK ! ! ! ]
+HERE=$(readlink -f $(dirname $0))
+BUILD_CONTEXT=$(readlink -f $HERE/../../../..)
+echo "Build context set to: $BUILD_CONTEXT"
 
 # Below is the config for setting the build context
 SERVICE_PATH=$BUILD_CONTEXT/atwater.git/api
@@ -20,7 +23,6 @@ DOCKERFILE_PATH=$SERVICE_PATH/docker/Dockerfile.$TYPE
 # this is a hack to ensure .dockerignore file gets used
 # in the future it will be possible to specificy the .dockerignore file as part of the build command
 cp -f $SERVICE_PATH/docker/.dockerignore $BUILD_CONTEXT/.dockerignore
-cd $BUILD_CONTEXT
 
 # this is the actual build command using Docker's CLI
 echo "Building $REPO/$SERVICE_NAME.$TYPE:$VERSION"
@@ -28,6 +30,6 @@ docker build -t $REPO/$SERVICE_NAME.$TYPE:$VERSION \
              -f $DOCKERFILE_PATH                   \
              --build-arg GIT_COMMIT=$GIT_COMMIT    \
              --build-arg DATE=$DATE                \
-             .
+             $BUILD_CONTEXT
 # clean up dockerignore hack
 rm -f $BUILD_CONTEXT/.dockerignore
