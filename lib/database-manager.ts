7531288f53883cb649e87ed26f244a15bb94ef47
Nicholas Leavitt <nicholas.leavitt@indexexchange.com>
2016-09-07 12:13:37 -0400

    Added test for db connection.

--- lib/database-manager.ts
+++ lib/database-manager.ts
@@ -1,3 +1,12 @@
+/**
+ * DatabaseManager
+ *
+ * The `DatabaseManager` object is a singleton which needs to be initialized before use. Import `DatabaseManager`
+ * from `./lib/database-manager` and call the method `DatabaseManager.initialize()`. The `DatabaseManager` object
+ * is an extension of the [Knex Query Builder](http://knexjs.org/#Builder) object, so refer to their documentation
+ * for usage.
+ *
+ */
 'use strict';
 
 import * as Knex from 'knex';
@@ -10,14 +19,14 @@ const Log: Logger = new Logger("DBMA");
 
 // DatabaseManager interface. As we can't implement this in the class directly it has to be separate. Not a big deal.
 interface IDatabaseManager extends Knex.QueryInterface {
-    initialize(): Promise<any>;
+    initialize(): Promise<{}>;
 }
 
 // The database manager class which extends Knex. After calling initialize, it properly extends the knex.js object.
 class DatabaseManager {
 
     // Load the configuration from environment and copy properties from Knex to itself.
-    public initialize(): Promise<any> {
+    public initialize(): Promise<{}> {
 
         Log.info('Initializing database connection...');
 
@@ -32,13 +41,19 @@ class DatabaseManager {
                 }
             };
 
-            let queryBuilder: Knex.QueryInterface = Knex(databaseConfig);
+            let queryBuilder: Knex = Knex(databaseConfig);
 
-            Object.assign(this, queryBuilder);
+            // Test DB Connection
+            queryBuilder.raw('SELECT 1')
+                .then(() => {
+                    Log.info('Database connection established successfully.');
 
-            Log.info('Database connection established successfully.');
-
-            resolve();
+                    Object.assign(this, queryBuilder);
+                    resolve();
+                })
+                .catch((err: ErrorEvent) => {
+                    reject(err);
+                });
         })
         .catch((err: ErrorEvent) => {
             Log.warn('Database failed to connect.');
