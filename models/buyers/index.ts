f57e321ee56cd3bac6b586ecb1941c60d8801688
Johnny El-Hayek <johnny.elhayek@indexexchange.com>
2016-09-08 17:19:27 -0400

    Added getBuyer method to model [ATW-74]

--- models/buyers/index.ts
+++ models/buyers/index.ts
@@ -5,8 +5,40 @@ import { Logger } from '../../lib/logger';
 
 const Log: Logger = new Logger('IXMB');
 
+// Interface for a buyer
+interface IBuyer {
+    userId: number;
+    dspIds: number[];
+    emailAddress: string;
+    firstName: string;
+    lastName: string;
+    companyName: string;
+}
+
+// A class for accessing and manipulating any data related to buyers
 class BuyerModel {
-    public static getDSPFromId(userId: number): Promise<any> {
+
+    // Create an object filled with information about the specified buyer
+    public static getBuyer(userId: number, dspId?: number): Promise<IBuyer> {
+        return BuyerModel.getInfoFromId(userId)
+            .then((info: any) => {
+                if (dspId) {
+                    return Object.assign({userId: userId, dspIds: [dspId]}, info[0]);
+                } else {
+                    return BuyerModel.getDSPsFromId(userId)
+                        .then((dsps: any) => {
+                            let dspArray: number[] = [];
+                            dsps.forEach((dsp: any) => {
+                                dspArray.push(dsp.dspid);
+                            });
+                            return Object.assign({userId: userId, dspIds: dspArray}, info[0]);
+                        });
+                }
+            });
+    }
+
+    // Get all dspIDs associated with a given userID
+    public static getDSPsFromId(userId: number): Promise<any> {
         return DatabaseManager.select('dspid')
             .from('ixmBuyers')
             .where('userid', userId)
@@ -16,6 +48,7 @@ class BuyerModel {
             });
     }
 
+    // Get all userIDs associated with a given DSP
     public static getIdsFromDSP(dspId: number): Promise<any> {
         return DatabaseManager.select('userid')
             .from('ixmBuyers')
@@ -26,6 +59,7 @@ class BuyerModel {
             });
     }
 
+    // Add a new buyer to the ixmBuyers table
     public static addBuyer(userId: number, dspId: number): Promise<any> {
         return DatabaseManager.insert({userId: userId, dspId: dspId})
             .into('ixmBuyers')
@@ -34,6 +68,18 @@ class BuyerModel {
                 throw err;
             });
     }
+
+    // Retrieve information tied to a given userID
+    private static getInfoFromId(userId: number): Promise<any> {
+        return DatabaseManager.select('emailAddress', 'firstName', 'lastName', 'companyName')
+            .from('users')
+            .where('userid', userId)
+            .catch((err: ErrorEvent) => {
+                Log.error(err.toString());
+                throw err;
+            });
+    }
+
 }
 
 export { BuyerModel }
