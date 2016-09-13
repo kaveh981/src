b5a2588ba843ec6985a2aac00f8a24e00e992020
Eusebio Olalde <eusebio.olalde@indexexchange.com>
2016-09-13 13:37:53 -0400

    Fixed start.sh to stop if node process exits on its own

--- api/etc/start.sh
+++ api/etc/start.sh
@@ -12,19 +12,20 @@ sigterm_handler() {
     kill -15 "$pid"
     wait "$pid"
   fi
-  exit 0;
+  exit 0
 }
 # setup handlers
 # on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
-trap 'kill ${!} && sigterm_handler' SIGTERM
+trap 'sigterm_handler' SIGTERM
 
 # start api
 node index.js &
 pid="$!"
-#exec npm start
 
 # wait forever so that container doesn't exit after spawning the processes
 while true
 do
-  tail -f /dev/null & wait ${!}
+  wait "$pid"
+  echo "Node process exited on its own, stopping container"
+  exit 10
 done
