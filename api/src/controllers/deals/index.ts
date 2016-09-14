40d4a3c135abb4d56d635fadb6bd002822b85d1b
Nicholas Leavitt <nicholas.leavitt@indexexchange.com>
2016-09-14 14:14:03 -0400

    Changed route slightly.

--- api/src/controllers/deals/index.ts
+++ api/src/controllers/deals/index.ts
@@ -4,7 +4,7 @@ import * as express from 'express';
 
 function Deals(router: express.Router): void {
 
-    router.route('/').get((req: express.Request, res: express.Response) => {
+    router.get('/', (req: express.Request, res: express.Response) => {
 
         res.sendPayload({'deals': 'Hi, I\'m Rick Harrison, and this is my pawn shop. I work here with my old man and \
         my son, "Big Hoss." Everything in here has a story and a price. One thing I\'ve learned after 21 years - you \
