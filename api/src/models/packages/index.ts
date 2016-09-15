166df6dd993ec16d6798186d7cefc268cbab849d
haiyang.sun <haiyang.sun@indexexchange.com>
2016-09-15 13:51:37 -0400

    Add JSDoc, syntax clean up  [ATW-161]

--- api/src/models/packages/index.ts
+++ api/src/models/packages/index.ts
@@ -1,47 +1,76 @@
 'use strict';
 
 import * as Promise from 'bluebird';
+
 import { DatabaseManager } from '../../lib/database-manager';
 import { Logger } from '../../lib/logger';
 
-const Log: Logger = new Logger('IXMPKG');
+const Log: Logger = new Logger('PKGM');
 
-// Interface of a package
+/**
+ * Interface for package of deal negotiation
+ */
 interface IPackage {
+    /** ID of the package */
     packageID: number;
+    /** ID of the package's owner, corresponding to users in database */
     ownerID: number;
+    /** Name of the package, unique value */
     name: string;
+    /** Description of the package */
     description: string;
+    /** Status of the packge, which could only be active, paused or deleted */
     status: string;
-    publicPackage: number;
+    /** Flag to define is the package viewable to public */
+    isPublic: number;
+    /** Start date of the package */
     startDate: string;
+    /** End date of the package */
     endDate: string;
+    /** Price of the package */
     price: number;
+    /** Projected amout of impressions for the package */
     impressions: number;
+    /** Project amount to be spend by the buyer */
     budget: number;
+    /** Auction type of the deal, which could only be first, second or fixed */
     auctionType: string;
+    /** Free text that both parties can edit to convene of specific deal conditions */
     terms: string;
+    /** Created date of the package */
     createDate: string;
+    /** Modified date of the package */
     modifiyDate: string;
+    /** Array of sectionsID associated with the package*/
     sections: number[];
 //  deals:number[]
 }
 
-// Class encapsulates functions related to ixmPackages
+/**
+ * Class encapsulates functions related to ixmPackages
+ */
 class PackageModel {
 
-    // Get package object includes associated sections
+    /**
+     * Get package object by ID
+     * @param packageID - the ID of the package
+     * @returns Returns a package object includes associated section IDs
+     */
     public static getPackageFromID (packageID: number): Promise<IPackage> {
         return PackageModel.getPackageInfo(packageID)
             .then((packageInfo: any) => {
                 return PackageModel.getPackageSections(packageID)
-                .then((sections: any) => {
-                    return Object.assign(packageInfo, {sections: sections});
-                });
+                    .then((sections: any) => {
+                        return Object.assign(packageInfo, {sections: sections});
+                    });
             });
     }
 
-    // Get package from its name and sections associated with the package
+    /**
+     * Get package object by name
+     * @param packageName - the unique name of the package
+     * @returns Returns a package object includes associated section IDs
+     */
     public static getPackageFromName (packageName: string): Promise<IPackage> {
         return DatabaseManager.select('packageID')
             .from('ixmPackages')
@@ -49,13 +78,17 @@ class PackageModel {
             .then((packageIDs: any) => {
                 return PackageModel.getPackageFromID(packageIDs[0].packageID);
             })
-            .catch((err: ErrorEvent) => {
+            .catch((err: Error) => {
                 Log.error(err.toString());
                 throw err;
             });
     }
 
-    // Get all packages with the given status, the return value is an array of package objects
+    /**
+     * Get list of objects by status
+     * @param packageStatus - status of the package, a enum value which could be active, paused or deleted
+     * @returns Returns an array of package objects by the given status
+     */
     public static getPackagesFromStatus (packageStatus: string): Promise<any> {
         return DatabaseManager.select('ixmPackages.packageID', 'ownerID', 'name', 'description', 'status', 'public',
                 'startDate', 'endDate', 'price', 'impressions', 'budget', 'auctionType', 'terms', 'createDate',
@@ -70,13 +103,17 @@ class PackageModel {
                 });
                 return packages;
             })
-            .catch((err: ErrorEvent) => {
+            .catch((err: Error) => {
                 Log.error(err.toString());
                 throw err;
             });
     }
 
-    // Get all packages with the given status, the return value is an array of package objects 
+    /**
+     * Get list of objects by owner
+     * @param packageOwner - ID of the package's owner
+     * @returns Returns an array of package objects by the given owner
+     */
     public static getPackagesFromOwner (packageOwner: number): Promise<any> {
         return DatabaseManager.select('ixmPackages.packageID', 'ownerID', 'name', 'description', 'status', 'public',
                 'startDate', 'endDate', 'price', 'impressions', 'budget', 'auctionType', 'terms', 'createDate',
@@ -91,20 +128,24 @@ class PackageModel {
                 });
                 return packages;
             })
-            .catch((err: ErrorEvent) => {
+            .catch((err: Error) => {
                 Log.error(err.toString());
                 throw err;
             });
     }
 
-    // Add a package and related mappings of sections 
+    /**
+     * Add a package and related mappings to database
+     * @param newPackage - package object includes corresponding sections
+     * @returns undefined
+     */
     public static addPackage (newPackage: any): Promise<any> {
         return DatabaseManager.insert({
                 ownerID: newPackage.ownerID,
                 name: newPackage.name,
                 description: newPackage.description,
                 status: newPackage.status,
-                public: newPackage.publicPackage,
+                public: newPackage.isPublic,
                 startDate: newPackage.startDate,
                 endDate: newPackage.endDate,
                 price: newPackage.price,
@@ -116,17 +157,21 @@ class PackageModel {
             })
             .into('ixmPackages')
             .then((newPackageID: any) => {
-                Promise.each(newPackage.sections, (sectionID: number) => {
+                return Promise.each(newPackage.sections, (sectionID: number) => {
                     PackageModel.insertPackageSectionMappings(newPackageID[0], sectionID);
                 });
             })
-            .catch((err: ErrorEvent) => {
+            .catch((err: Error) => {
                 Log.error(err.toString());
                 throw err;
             });
     }
 
-    // Get package information from ID
+    /**
+     * Get package information by package ID
+     * @param packageID - the ID of the package
+     * @returns Returns an object include all the information of the package 
+     */
     private static getPackageInfo (packageID: number): Promise<any> {
         return DatabaseManager.select('packageID', 'ownerID', 'name', 'description', 'status', 'public', 'startDate',
                 'endDate', 'price', 'impressions', 'budget', 'auctionType', 'terms', 'createDate', 'modifyDate')
@@ -135,13 +180,17 @@ class PackageModel {
             .then((packageInfo: any) => {
                 return packageInfo[0];
             })
-            .catch((err: ErrorEvent) => {
+            .catch((err: Error) => {
                 Log.error(err.toString());
                 throw err;
             });
     }
 
-    // Get array of sections associated to a package
+    /**
+     * Get corresponding section IDs by package ID
+     * @param packageID - the ID of the package
+     * @returns Returns an array of section IDs
+     */
     private static getPackageSections (packageID: number): Promise<number []> {
         return DatabaseManager.select('sectionID')
             .from('ixmPackageSectionMappings')
@@ -153,20 +202,25 @@ class PackageModel {
                 });
                 return sectionIDs;
             })
-            .catch((err: ErrorEvent) => {
+            .catch((err: Error) => {
                 Log.error(err.toString());
                 throw err;
             });
     }
 
-    // Insert into ixmPackageSectionMappings
+    /**
+     * Insert package and section mappings into database
+     * @param packageID - the ID of the package
+     * @param {number} - the ID of the section
+     * @returns Returns a array with number 0 inside
+     */
     private static insertPackageSectionMappings (packageID: number, sectionID: number): Promise<any> {
          return DatabaseManager.insert({
                packageID: packageID,
                sectionID: sectionID
             })
             .into('ixmPackageSectionMappings')
-            .catch((err: ErrorEvent) => {
+            .catch((err: Error) => {
                 Log.error(err.toString());
                 throw err;
             });
