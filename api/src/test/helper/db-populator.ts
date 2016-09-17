46067dafad825c3b0d0873665f8a147d7983659f
Eusebio Olalde <eusebio.olalde@indexexchange.com>
2016-09-17 00:31:18 -0400

    Refactored DataPopulator

--- api/src/test/helper/db-populator.ts
+++ api/src/test/helper/db-populator.ts
@@ -1,56 +1,91 @@
 // Database pupulator class to insert data for testing purposes
 
 import * as Promise from 'bluebird';
+// import * as jsf from 'json-schema-faker';
+const jsf = require('json-schema-faker');
+// import * as faker from 'faker';
+const faker = require('faker');
 
 import { DatabaseManager } from './database-manager';
 import { Logger          } from './logger';
-import { dataGenerator } from './data-generator';
-import { Config } from './config-loader';
+import { ConfigLoader } from './config-loader';
+
+const logger = new Logger("DPOP");
+
 
 export class DatabasePopulator {
 
     constructor (
-        private dbm: DatabaseManager = new DatabaseManager(Config),
-        private logger = new Logger("DBPO"),
-        private dg = dataGenerator
+        private dbm: DatabaseManager,
+        private config: ConfigLoader
     ) {
+        faker.locale = "es_MX";
     }
 
     public initialize (): Promise<void> {
         return this.dbm.initialize()
             .then(() => {
-                this.logger.info("DatabasePopulator successfully initialized")
+                logger.info("DatabasePopulator successfully initialized")
             })
             .catch((err) => {
-                this.logger.error(err)
+                logger.error(err)
             });
     }
 
     public shutdown (): void {
         this.dbm.shutdown();
-        this.logger.info("DatabasePopulator gracefully shutdown");
+        logger.info("DatabasePopulator gracefully shutdown");
     }
 
-    public insertNewBuyer (): Promise<void> {
-        let newBuyerData = this.dg.generateNewBuyer();
-        let newID;
-        return this.dbm.insert(newBuyerData)
+    public newUser (userFields?: any): Promise<any> {
+        let newUserData;
+        if (!userFields) {
+            let schema  = this.config.get('data-gen/new-user-schema');
+            newUserData = jsf(schema);
+        }
+        else {
+            newUserData = userFields;
+        }
+        return this.dbm.insert(newUserData)
             .into("users")
             .then((newBuyerID: number[]) => {
-                newID = newBuyerID[0];
-                this.logger.info("Added new Buyer with userID: " + newID);
-                return this.dbm.update("status", "A")
-                    .from("users")
-                    .where('userID', '=', newID);
-            })
-            .then(() => {
-                this.logger.info("Updated UserType");
+                newUserData.userID = newBuyerID[0];
+                logger.info(`Added new User: ${newUserData.userID} `);
+                return newUserData;
             })
             .catch((err) => {
-                this.logger.error(err);
+                logger.error(err);
                 throw err;
             });
     }
-}
 
-export const dbPopulator: DatabasePopulator = new DatabasePopulator();
+    public newBuyer (): Promise<any> {
+        let schema = this.config.get('data-gen/new-buyer-schema');
+        let newBuyerData = jsf(schema);
+        return this.newUser(newBuyerData.user)
+            .then((newUserData: any) => {
+                newBuyerData.user.userID = newUserData.userID;
+                return this.dbm
+                    .insert({userID: newBuyerData.user.userID, dspID: newBuyerData.dspID})
+                    .into("ixmBuyers")
+                    .catch((e) => { throw e });
+            })
+            .then(() => {
+                logger.info(`Added new Buyer with userID: ${newBuyerData.user.userID}, dspID: ${newBuyerData.dspID}`);
+                return newBuyerData;
+            })
+            .catch((err) => {
+                logger.error(err);
+                throw err;
+            });
+    }
+
+    /*public insertNewPub (): Promise<number> {
+        //TODO Maybe you don't even need it
+    }
+
+    public insertNewPackage (): Promise<number> {
+        //TODO
+    }*/
+
+}
