3125c6e7e37a089fa6b35c5ba3746eefab3f6a4d
Johnny El-Hayek <johnny.elhayek@indexexchange.com>
2016-09-19 16:28:15 -0400

    Updated buyer model and deals route [ATW-158]

--- api/src/controllers/deals/index.ts
+++ api/src/controllers/deals/index.ts
@@ -7,40 +7,50 @@ import { Logger } from '../../lib/logger';
 import { DatabaseManager } from '../../lib/database-manager';
 import { Injector } from '../../lib/injector';
 
-import { PackageModel } from '../../models/package-model';
-//import { UserModel } from '../../models/user-model';
+import { PackageManager } from '../../models/package/package-manager';
+import { PackageModel } from '../../models//package/package-model';
+
+import { UserManager } from '../../models/user/user-manager';
+import { UserModel } from '../../models/user/user-model';
 
 const databaseManager = Injector.request<DatabaseManager>('DatabaseManager');
+const packageManager = new PackageManager(databaseManager);
+const userManager: UserManager = new UserManager(databaseManager);
 
 const Log: Logger = new Logger('DEAL');
 
+/**
+ * Function that takes care of all /deal routes
+ */
 function Deals(router: express.Router): void {
 
+    /**
+     * GET request to get all available Deals
+     */
     router.get('/', (req: express.Request, res: express.Response) => {
         let availablePackages = [];
-        PackageModel.getPackagesFromStatus('active')
+        // Get all packages with an 'active' status
+        packageManager.getPackagesFromStatus('active')
             .then((activePackages: any) => {
                 Promise.each(activePackages, (activePackage: any) => {
                     let startDate: Date = new Date(activePackage.startDate);
                     let endDate: Date = new Date(activePackage.endDate);
                     let today: Date = new Date(Date.now());
                     let zeroDate: string = '0000-00-00';
-
+                    // Set all date "hours" to be 0 to be able to just compare the dates alone
                     startDate.setHours(0, 0, 0, 0);
                     endDate.setHours(0, 0, 0, 0);
                     today.setHours(0, 0, 0, 0);
-
-                    // let user = new UserModel(activePackage.ownerID, databaseManager);
-
-                    // return user.populate()
-                    //     .then(() => {
-                    //         if (user.userStatus === 'A'
-                    //             && (startDate <= today || activePackage.startDate === zeroDate)
-                    //             && (endDate >= today || activePackage.endDate === zeroDate)
-                    //             && activePackage.sections.length > 0) {
-                    //                 availablePackages.push(activePackage);
-                    //         }
-                    //     });
+                    // Make sure a package is owned by an active user, has valid dates, and has at least one associated deal section
+                    userManager.fetchUserFromId(activePackage.ownerID)
+                        .then((user: UserModel) => {
+                            if (user.userStatus === 'A'
+                                && (startDate <= today || activePackage.startDate === zeroDate)
+                                && (endDate >= today || activePackage.endDate === zeroDate)
+                                && activePackage.sections.length > 0) {
+                                     availablePackages.push(activePackage);
+                            }
+                        });
                 })
                 .then(() => {
                     if (availablePackages.length === 0) {
---
+++ api/src/models/buyer/buyer-manager.ts
@@ -0,0 +1,116 @@
+'use strict';
+
+import * as Promise from 'bluebird';
+
+import { DatabaseManager } from '../../lib/database-manager';
+import { Logger } from '../../lib/logger';
+import { BuyerModel } from './buyer-model';
+
+const Log: Logger = new Logger('mBYR');
+
+/** Buyer model manager */
+class BuyerManager {
+
+    /** Internal dbm  */
+    private dbm: DatabaseManager;
+
+    /**
+     * Constructor
+     * @param database - An instance of the database manager.
+     */
+    constructor(database: DatabaseManager){
+        this.dbm = database;
+    }
+
+    /**
+     * Returns a buyer model from a userID and an optional dspID
+     * @param userId - The userID of the buyer we want information from.
+     * @param dspId - A dspID associated with the buyer (It is unclear whether we need this as an optional parameter or not)
+     */
+    public fetchBuyer(userId: number, dspId?: number): Promise<BuyerModel> {
+        return this.getInfoFromId(userId)
+            .then((info: any) => {
+                if (dspId) {
+                    let buyer: BuyerModel = Object.assign({userId: userId, dspIds: [dspId]}, info[0]);
+                    return new BuyerModel(buyer);
+                } else {
+                    return this.getDSPsFromId(userId)
+                        .then((dsps: any) => {
+                            let dspArray: number[] = [];
+                            dsps.forEach((dsp: any) => {
+                                dspArray.push(dsp.dspid);
+                            });
+                            let buyer: BuyerModel = Object.assign({userId: userId, dspIds: dspArray}, info[0]);
+                            return new BuyerModel(buyer);
+                        });
+                }
+            });
+    }
+
+    /**
+     * Returns a boolean indicating whether a userID is associated with an IXM Buyer.
+     * @param userId - The ID of the user we want to check against the ixmBuyers table.
+     */
+    public isIXMBuyer(userId: number): Promise<boolean> {
+        return this.dbm.select().from('ixmBuyers').where('userID', userId).limit(1)
+            .then((rows: any) => {
+                return rows.length !== 0;
+            })
+            .catch((err: Error) => {
+                Log.error(err);
+                throw err;
+            });
+    }
+
+    /**
+     * Returns all userIDs associated with a given DSP
+     * @param dspId - The ID of the DSP we want to obtain users for.
+     */
+    public getIdsFromDSP(dspId: number): Promise<any> {
+        return this.dbm.select('userid')
+            .from('ixmBuyers')
+            .where('dspid', dspId)
+            .catch((err: Error) => {
+                Log.error(err);
+                throw err;
+            });
+    }
+
+    /**
+     * Adds a new buyer to the ixmBuyers table (Until we design ContactInfo, this function just takes two ID params)
+     */
+    public saveBuyer(userId: number, dspId: number): Promise<any> {
+        return this.dbm.insert({userId: userId, dspId: dspId})
+            .into('ixmBuyers')
+            .catch((err: Error) => {
+                Log.error(err);
+                throw err;
+            });
+    }
+
+    /** 
+     * Helper function for getting all dspIDs associated with a given userID 
+     */
+    private getDSPsFromId(userId: number): Promise<any> {
+        return this.dbm.select('dspid')
+            .from('ixmBuyers')
+            .where('userid', userId)
+            .catch((err: Error) => {
+                Log.error(err);
+                throw err;
+            });
+    }
+
+    /**
+     * Helper function for retrieving contact information of a user (Might be updated to get this info from ixmBuyers?)
+     */
+    private getInfoFromId(userId: number): Promise<any> {
+        return this.dbm.select('emailAddress', 'firstName', 'lastName', 'companyName')
+            .from('users')
+            .where('userid', userId)
+            .catch((err: Error) => {
+                Log.error(err);
+                throw err;
+            });
+    }
+}
