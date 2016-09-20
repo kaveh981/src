d30ba1ba66decb874f4a3669d21fdb70d443eb84
Jeremy Heritier <jeremy.heritier@indexexchange.com>
2016-09-20 10:54:37 -0400

    Added all attributes of the deal [ATW-160]

--- api/src/models/deal/deal-model.ts
+++ api/src/models/deal/deal-model.ts
@@ -5,8 +5,35 @@ interface IDealModel {
     /** The deal's unique internal identifier */
     id: number;
 
+    /** The publisher offering the deal */
+    userId: number;
+
+    /** The DSP buying the deal */
+    dspId: number;
+
+    /** A descriptive name for the deal */
+    name: string;
+
+    /** The auction type under which the deal is operating */
+    auctionType: string;
+
+    /** The reserved rate of the deal */
+    price: number;
+
     /** The current status of the deal */
     status: string;
+
+    /** The first day when the deal will serve */
+    startDate: string;
+
+    /** The last day when the deal will serve */
+    endDate: string;
+
+    /** The external ID the DSP must use when they bid with the deal */
+    externalID : string;
+
+    /** The sections where the deal is eligible to serve */
+    sections: number[];
 
 }
 
@@ -19,8 +46,35 @@ class DealModel implements DealModel {
     /** The deal's unique internal identifier */
     public id: string;
 
+    /** The publisher offering the deal */
+	public userId: number;
+
+    /** The DSP buying the deal */
+	public dspId: number;
+
+    /** A descriptive name for the deal */
+	public name: string;
+
+    /** The auction type under which the deal is operating */
+	public auctionType: string;
+
+    /** The reserved rate of the deal */
+	public price: number;
+
     /** The current status of the deal */
-    public status: string;
+	public status: string;
+
+    /** The first day when the deal will serve */
+	public startDate: string;
+
+    /** The last day when the deal will serve */
+	public endDate: string;
+
+    /** The external ID the DSP must use when they bid with the deal */
+	public externalID : string;
+
+    /** The sections where the deal is eligible to serve */
+	public sections: number[];
 
     /**
      * Constructor
