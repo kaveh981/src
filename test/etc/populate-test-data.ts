d9f781aaf6f54ab7bd95ec6c48f8233b76a17e04
Eusebio Olalde <eusebio.olalde@indexexchange.com>
2016-09-20 12:02:16 -0400

    Symlinks to api/src added

--- test/etc/populate-test-data.ts
+++ test/etc/populate-test-data.ts
@@ -1,21 +1,24 @@
 // Script to auto-populate Viper2 with data relevant to IXM Buyer API
 
 import { DatabasePopulator } from '../helper/db-populator';
-import { Logger }      from '../../api/src/lib/logger';
-import { ConfigLoader }      from '../../api/src/lib/config-loader';
-import { DatabaseManager } from '../../api/src/lib/database-manager';
+import { Logger }      from '../lib/logger';
+import { ConfigLoader }      from '../lib/config-loader';
+import { DatabaseManager } from '../lib/database-manager';
 
 const Log= new Logger("TEST");
-const Config = new ConfigLoader('../test/config');
+const Config = new ConfigLoader('../../../test/config');
 const dbm = new DatabaseManager(Config);
 const dbPopulator= new DatabasePopulator(dbm, Config);
 
-dbPopulator.initialize()
+dbm.initialize()
     .then(() => {
         return dbPopulator.newPub();
     })
-    .then(dbPopulator.shutdown.bind(dbPopulator))
+    .then((newPubData) => {
+        return dbPopulator.newSite(newPubData.user.userID);
+    })
+    .then(dbm.shutdown.bind(dbm))
     .catch((err) => {
         Log.error(err);
-        dbPopulator.shutdown();
+        dbm.shutdown();
     });
--- test/helper/db-populator.ts
+++ test/helper/db-populator.ts
@@ -4,12 +4,11 @@ import * as Promise from 'bluebird';
 const jsf = require('json-schema-faker');
 const faker = require('faker');
 
-import { DatabaseManager } from '../../api/src/lib/database-manager';
-import { Logger          } from '../../api/src/lib/logger';
-import { ConfigLoader    } from '../../api/src/lib/config-loader';
+import { DatabaseManager } from '../lib/database-manager';
+import { ConfigLoader    } from '../lib/config-loader';
+import { Logger          } from '../lib/logger';
 
 const Log = new Logger("DPOP");
-
 
 class DatabasePopulator {
 
@@ -25,85 +24,94 @@ class DatabasePopulator {
         faker.locale = "es_MX";
     }
 
-    public initialize (): Promise<void> {
-        return this.dbm.initialize()
-            .then(() => {
-                Log.info("DatabasePopulator successfully initialized")
-            })
-            .catch((err) => {
-                Log.error(err)
-            });
+    private genDataObj<T> (schemaName: string): T {
+        let schema = this.config.get('data-gen/' + schemaName);
+        return jsf(schema);
     }
 
-    public shutdown (): void {
-        this.dbm.shutdown();
-        Log.info("DatabasePopulator gracefully shutdown");
+    private extendData<T> (data: any, generatedData: T): T {
+        Object.assign(generatedData, data);
+        return generatedData;
     }
 
-    public newUser (userFields?: any): Promise<any> { //TODO interface userFields
-        let newUserData;
-        if (!userFields) {
-            let schema  = this.config.get('data-gen/new-user-schema');
-            newUserData = jsf(schema);
+    public newUser (userFields?: INewUserData): Promise<INewUserData> {
+        let newUserData: INewUserData = this.genDataObj<INewUserData>('new-user-schema');
+
+        if (userFields) {
+            newUserData = this.extendData<INewUserData>(userFields, newUserData);
         }
-        else {
-            newUserData = userFields;
-        }
-        return this.dbm.insert(newUserData)
+
+        return this.dbm.insert(newUserData, "userID")
             .into("users")
             .then((newBuyerID: number[]) => {
                 newUserData.userID = newBuyerID[0];
                 Log.info(`Added new User: ${newUserData.userID} `);
                 return newUserData;
             })
-            .catch((err) => {
-                Log.error(err);
-                throw err;
+            .catch((e) => {
+                throw e;
             });
     }
 
-    public newBuyer (): Promise<any> { //TODO interface NewBuyerData
-        let schema = this.config.get('data-gen/new-buyer-schema');
-        let newBuyerData = jsf(schema);
+    public newBuyer (): Promise<INewBuyerData> {
+        let newBuyerData: INewBuyerData = this.genDataObj<INewBuyerData>('new-buyer-schema');
         return this.newUser(newBuyerData.user)
-            .then((newUserData: any) => {
+            .then((newUserData: INewUserData) => {
                 newBuyerData.user.userID = newUserData.userID;
                 return this.dbm
                     .insert({userID: newBuyerData.user.userID, dspID: newBuyerData.dspID})
                     .into("ixmBuyers")
-                    .catch((e) => { throw e });
             })
             .then(() => {
                 Log.info(`Added new Buyer with userID: ${newBuyerData.user.userID}, dspID: ${newBuyerData.dspID}`);
                 return newBuyerData;
             })
-            .catch((err) => {
-                Log.error(err);
-                throw err;
+            .catch((e) => {
+                throw e;
             });
     }
 
-    public newPub (): Promise<any> { //TODO Interface NewPubData
-        let schema = this.config.get('data-gen/new-pub-schema');
-        let newPubData = jsf(schema);
+    public newPub (): Promise<INewPubData> {
+        let newPubData = this.genDataObj<INewPubData>('new-pub-schema');
         return this.newUser(newPubData.user)
-            .then((newUserData: any) => {
-                let publisherData = newPubData.publisher;
+            .then((newUserData: INewUserData) => {
+                let publisherData = <any>newPubData.publisher;
                 publisherData.userID = newUserData.userID;
                 newPubData.user.userID = newUserData.userID;
                 return this.dbm
                     .insert(publisherData)
                     .into("publishers");
             })
-            .catch((err) => {
-                Log.error(err);
-                throw err;
+            .then(() => {
+                return newPubData;
+            })
+            .catch((e) => {
+                throw e;
             });
     }
 
-    /*public insertNewPackage (): Promise<number> {
-        //TODO
-    }*/
+    public newSite (userID: number): Promise<INewSiteData> {
+        let newSiteData = this.genDataObj<INewSiteData>('new-site-schema');
+        newSiteData.userID = userID;
+
+        return this.dbm
+            .insert(newSiteData)
+            .into("sites")
+            .catch((e) => {
+                Log.error(e);
+                throw e;
+            });
+    }
+
+
+    public newSection (siteIDs: number[]): Promise<INewSectionData> {
+        let newSectionData = this.genDataObj<ISection>('new-section-schema');
+        
+    }
+
+    public newPackage (ownerID: number, sectionIDs: number[]): Promise<INewPackageData> {
+        //TODO newPackage method
+    }
 
 }
 
