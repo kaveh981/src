c84cfa637278d2a6991e5f0d07b9a30c36b4a9a5
Kaveh Fereidouni <kaveh.fereidouni@indexexchange.com>
2016-09-29 13:42:19 -0400

    add comment for method [ATW-163]

--- test/helper/api-helper.ts
+++ test/helper/api-helper.ts
@@ -56,6 +56,9 @@ class ApiHelper {
         this.userID = _userID;
     }
 
+    /**
+     * Clear buyer userID from the request header
+     */
     public clearBuyerUserID() {
         this.userID = undefined;
     }
