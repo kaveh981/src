a94dd84f5e61270f99a15e84b8f4505def0194e5
Nicholas Leavitt <nicholas.leavitt@indexexchange.com>
2016-10-03 14:19:20 -0400

    [ATW-261] I don't understand where this went.

--- api/test/lib-test/database-manager.ts
+++ api/test/lib-test/database-manager.ts
@@ -9,34 +9,31 @@ import { ConfigLoader } from '../../src/lib/config-loader';
 const config = new ConfigLoader();
 Injector.put(config, 'ConfigLoader');
 
-import { Validator } from '../../src/lib/validator';
+import { DatabaseManager } from '../../src/lib/database-manager';
 
-const validator = new Validator();
+const databaseManager = new DatabaseManager(config);
 
-test('Validator Test', (assert: test.Test) => {
-    assert.plan(2);
+/**
+ * Unit test of database manager initialization
+ */
+test('Database Selection Test', (assert: test.Test) => {
+    assert.plan(1);
 
-    validator.initialize()
+    databaseManager.initialize()
             .then(() => {
-                let goodRat: any = {
-                    name: 'Meow',
-                    diet: ['human meat', 'frogs legs']
-                };
-
-                let badRat: any = {
-                    name: 'Woof',
-                    diet: 'rabbits',
-                    attitude: 'bad'
-                };
-
-                assert.equal(validator.validate(goodRat, 'Rat').success, 1, 'Good rat should pass validation.');
-                assert.equal(validator.validate(badRat, 'Rat').errors.length, 2,
-                    'Bad rat should have two failures, bad diet and bad attitude.');
-
+                return databaseManager.select().from('users').limit(1)
+                    .then((rows: any) => {
+                        assert.equal(rows.length, 1, 'Selected one user from the users table.');
+                    })
+                    .catch((err: Error) => {
+                        assert.fail('User selection failed ' + err);
+                    });
             })
             .catch((err: Error) => {
-                assert.fail('Initialization failed with error: ' + err);
-                assert.end();
+                assert.fail('Database failed with error: ' + err);
+            })
+            .finally(() => {
+                databaseManager.shutdown();
             });
 
 });
