653405a5d6f23c4a48ff6970d432fa67e0e61218
haiyang.sun <haiyang.sun@indexexchange.com>
2016-10-07 14:49:07 -0400

    Implement test module for authenticaton[ATW-253]

--- test/e2e/auth/auth.test.ts
+++ test/e2e/auth/auth.test.ts
@@ -1,6 +1,5 @@
 'use strict';
 
-import * as test from 'tape';
 import * as express from 'express';
 import * as Promise from 'bluebird';
 import * as testFramework  from 'testFramework';
@@ -11,98 +10,98 @@ import { Test     } from 'tape';
 
 const dataSetup = Injector.request<testFramework.IDataSetup>('DataSetup');
 
-import { DatabasePopulator } from '../../helper/database-populator';
-const databasePopulator = Injector.request<DatabasePopulator>('DatabasePopulator');
-
 import { ConfigLoader } from '../../lib/config-loader';
 const config = Injector.request<ConfigLoader>('ConfigLoader');
 
-function authTest(apiHelper: testFramework.IApiHelper): void {
+import { DatabasePopulator } from '../../helper/database-populator';
+const databasePopulator = Injector.request<DatabasePopulator>('DatabasePopulator');
 
-    test('Auth Test', (t: Test) => {
+/**
+ * Reusable Test for buyer authentication 
+ * @param {Test} t
+ * @param {testFramework.IApiHelper} apiHelper
+ * @param {number} newBuyerID
+ */
+function authTest(t: Test, apiHelper: testFramework.IApiHelper, newBuyerID: number): void {
 
-        const tables: string[] = ['publishers', 'users', 'ixmBuyers'];
-        let newBuyer: INewBuyerData;
-        let newPub: INewPubData;
-        let buyerIDKey: string = config.get('auth').header;
+    const tables: string[] = ['publishers', 'users'];
+    let newPub: INewPubData;
+    let buyerIDKey: string = config.get('auth').header;
 
-        t.test('setup', (assert: test.Test) => {
-            Promise.coroutine(function* (): any {
-                    yield app.boot();
-                    for (let i = 0; i < tables.length; i += 1) {
-                        let table = tables[i];
-                        yield dataSetup.backupTable(table);
-                        yield dataSetup.clearTable(table);
-                    }
-                    newBuyer = yield databasePopulator.newBuyer();
-                    newPub = yield databasePopulator.newPub();
-                })()
-                .finally(() => {
-                    assert.end();
-                });
-        });
-
-         t.test('ATW_AUTH_1 when valid parameters passed in', (assert: Test) => {
-            apiHelper.setReqOpts({headers: {[buyerIDKey]: newBuyer.user.userID}});
-            apiHelper.sendRequest()
-                .then((res: any) => {
-                    assert.equal(res.httpStatusCode, 200, 'It should return status code 200, returned message is: ' + res.body.message);
-                })
-                .finally(() => {
-                    assert.end();
-                });
-            apiHelper.setReqOpts({headers: {}});
-        });
-
-        t.test('ATW_AUTH_2 when buyerID in the header is not ixmBuyerID', (assert: Test) => {
-            apiHelper.setReqOpts({headers: {[buyerIDKey]: '`' + newPub.user.userID }});
-            apiHelper.sendRequest()
-                .then((res: any) => {
-                    assert.equal(res.httpStatusCode, 401, 'It should return status code 401, returned message is: ' + res.body.message);
-                })
-                .finally(() => {
-                    assert.end();
-                });
-            apiHelper.setReqOpts({headers: {}});
-        });
-
-        t.test('ATW_AUTH_3 when buyerID in the header is a non int', (assert: Test) => {
-            apiHelper.setReqOpts({headers: {[buyerIDKey]: '`' + newBuyer.user.userID + '`'}});
-            apiHelper.sendRequest()
-                .then((res: any) => {
-                    assert.equal(res.httpStatusCode, 401, 'It should return status code 401, returned message is: ' + res.body.message);
-                })
-                .finally(() => {
-                    assert.end();
-                });
-            apiHelper.setReqOpts({headers: {}});
-        });
-
-        t.test('ATW_AUTH_4 when buyerID not exists on the header', (assert: Test) => {
-            apiHelper.sendRequest()
-                .then((res: any) => {
-                    assert.equal(res.httpStatusCode, 401, 'It should return status code 401, returned message is: ' + res.body.message);
-                })
-                .finally(() => {
-                    assert.end();
-                });
-        });
-
-        t.test('teardown', (assert: test.Test) => {
-            Promise.coroutine(function* (): any {
+    t.test('setup', (assert: Test) => {
+        Promise.coroutine(function* (): any {
                 for (let i = 0; i < tables.length; i += 1) {
                     let table = tables[i];
-                    yield dataSetup.restoreTable(table);
+                    yield dataSetup.backupTable(table, '_backup_auth');
                 }
-                app.shutdown();
-
+                newPub = yield databasePopulator.newPub();
             })()
             .finally(() => {
                 assert.end();
             });
-        });
-
     });
+
+    t.test('AUTH_1 when valid parameters passed in', (assert: Test) => {
+        apiHelper.setReqOpts({headers: {[buyerIDKey]: newBuyerID}});
+
+        apiHelper.sendRequest()
+        .then((res: any) => {
+            assert.equal(res.httpStatusCode, 200, 'It should return status code 200, returned message is: ' + res.body.message);
+        })
+        .finally(() => {
+            assert.end();
+        });
+        apiHelper.setReqOpts({headers: {}});
+    });
+    t.test('AUTH_2 when buyerID in the header is not ixmBuyerID', (assert: Test) => {
+
+        apiHelper.setReqOpts({headers: {[buyerIDKey]: '`' + newPub }});
+        apiHelper.sendRequest()
+            .then((res: any) => {
+                assert.equal(res.httpStatusCode, 401, 'It should return status code 401, returned message is: ' + res.body.message);
+            })
+            .finally(() => {
+                assert.end();
+            });
+        apiHelper.setReqOpts({headers: {}});
+    });
+
+    t.test('AUTH_3 when buyerID in the header is a non int', (assert: Test) => {
+        apiHelper.setReqOpts({headers: {[buyerIDKey]: '`' + newBuyerID + '`'}});
+        apiHelper.sendRequest()
+            .then((res: any) => {
+                assert.equal(res.httpStatusCode, 401, 'It should return status code 401, returned message is: ' + res.body.message);
+            })
+            .finally(() => {
+                assert.end();
+            });
+        apiHelper.setReqOpts({headers: {}});
+    });
+
+    t.test('AUTH_4 when buyerID not exists on the header', (assert: Test) => {
+        apiHelper.sendRequest()
+            .then((res: any) => {
+                assert.equal(res.httpStatusCode, 401, 'It should return status code 401, returned message is: ' + res.body.message);
+            })
+            .finally(() => {
+                assert.end();
+            });
+    });
+
+    t.test('teardown', (assert: Test) => {
+        Promise.coroutine(function* (): any {
+            for (let i = 0; i < tables.length; i += 1) {
+                let table = tables[i];
+                yield dataSetup.restoreTable(table, '_backup_auth');
+            }
+            app.shutdown();
+
+        })()
+            .finally(() => {
+                assert.end();
+            });
+    });
+
 }
 
 export { authTest }
