1c07a947a121ec967d7d8874adf19cd96ee8185d
Eusebio Olalde <eusebio.olalde@indexexchange.com>
2016-10-12 13:39:04 -0400

    [ATW-215] Removed current branch reset from build-args.sh

--- build-args.sh
+++ build-args.sh
@@ -3,12 +3,9 @@
 TAG=${VERSION:-newest}
 
 git fetch --all \
-  && git reset --hard origin/$(parse_git_branch) \
-  && git pull \
   && git checkout master \
   && git pull \
   && git checkout $TAG \
-  && git pull \
   || exit 10
 
 GIT_COMMIT=$(git rev-parse --short HEAD)
