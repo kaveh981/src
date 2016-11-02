8aa82372ea837f60f58187d648a1710eca1ae173
Kaveh Fereidouni <kaveh.fereidouni@indexexchange.com>
2016-11-02 09:50:34 -0400

    [ATW-242] add uri param support for CVT

--- test/test-cases/common/validation.test.ts
+++ test/test-cases/common/validation.test.ts
@@ -4,7 +4,7 @@ import * as test from 'tape';
 
 import { Injector } from '../../src/lib/injector';
 import { APIRequestManager } from '../../src/lib/request-manager';
-import { ConfigLoader    } from '../../src/lib/config-loader';
+import { ConfigLoader } from '../../src/lib/config-loader';
 import { DataSetup } from '../../src/lib/data-setup';
 import { Logger } from '../../src/lib/logger';
 
@@ -19,10 +19,11 @@ const dataSetup = Injector.request<DataSetup>('DataSetup');
  * @param route - The route that we are calling.
  * @param verb - The verb that we are using for the api.
  * @param setup - The setup function that populate required tables.
- * @param validationParams - The api request parameters.
+ * @param validationParams - The api request body parameters with their respected options.
+ * @param uriParams - An object that has the parameters we want to send in the route uri with their respected options.
  * @returns an array of test cases functions.
  */
-function validationTest(route: string, verb: string, setup: Function, validationParams: {}) {
+function validationTest(route: string, verb: string, setup: Function, validationParams: {}, uriParams?: {}) {
 
     let tests = [];
 
@@ -39,30 +40,85 @@ function validationTest(route: string, verb: string, setup: Function, validation
             // create a function that has all the api calls for invalid cases we provide
             let testFunction = async (t: test.Test) => {
                 let setupRes = await setup();
-                let requestParams = parseRequestParams(validationParams, setupRes);
+                let validRequestParams = parseRequestParams(validationParams, setupRes);
+                let validUriParams = parseRequestParams(uriParams, setupRes);
+                let routeParam = route;
+                for (let validParam in validUriParams) {
+                    if (validUriParams.hasOwnProperty(validParam)) {
+                        routeParam += `/${validUriParams[validParam]}`;
+                    }
+                }
                 t.plan(cases.length);
                 // loop through cases
                 for (let i = 0; i < cases.length; i++) {
 
-                    requestParams[property] = cases[i].input;
-                    let res = await apiRequest[verb.toLowerCase()](route, requestParams, setupRes.userID);
+                    validRequestParams[property] = cases[i].input;
+                    let res = await apiRequest[verb.toLowerCase()](routeParam, validRequestParams, setupRes.userID);
                     t.equal(res.status, cases[i].expect || 400);
                     // clear table and run setup if the api call succeed for any reason to start with fresh data for the next test
                     if (res.status === 200) {
                         await dataSetup.clearTables();
                         setupRes = await setup();
-                        requestParams = parseRequestParams(validationParams, setupRes);
+                        validRequestParams = parseRequestParams(validationParams, setupRes);
                     }
 
                 }
                 // to test missing param
             };
 
-            tests.push((assert: test.Test) => {return testFunction(assert); });
+            tests.push((assert: test.Test) => { return testFunction(assert); });
 
         }
 
     }
+
+    // loop trough uriParams 
+    for (let param in uriParams) {
+
+        if (uriParams.hasOwnProperty(param)) {
+
+            let cases = configLoader.get(`common-validation/${uriParams[param].type}`);
+            if (uriParams[param].extraCases && uriParams[param].extraCases.length > 0) {
+                cases = cases.concat(uriParams[param].extraCases);
+            }
+
+            // create a function that has all the api calls for invalid cases we provide
+            let testFunction = async (t: test.Test) => {
+                let setupRes = await setup();
+                let validRequestParams = parseRequestParams(validationParams, setupRes);
+                let validUriParams = parseRequestParams(uriParams, setupRes);
+                let routeParam = route;
+                t.plan(cases.length);
+                // loop through cases
+                for (let i = 0; i < cases.length; i++) {
+
+                    validUriParams[param] = cases[i].input;
+                    routeParam = route;
+                    for (let validParam in validUriParams) {
+
+                        if (validUriParams.hasOwnProperty(validParam)) {
+                            routeParam += `/${validUriParams[validParam]}`;
+                        }
+                    }
+                    let res = await apiRequest[verb.toLowerCase()](routeParam, validRequestParams, setupRes.userID);
+                    t.equal(res.status, cases[i].expect || 400);
+                    // clear table and run setup if the api call succeed for any reason to start with fresh data for the next test
+                    if (res.status === 200) {
+                        await dataSetup.clearTables();
+                        setupRes = await setup();
+                        validUriParams = parseRequestParams(uriParams, setupRes);
+                    }
+
+                }
+                // to test missing param
+            };
+
+            tests.push((assert: test.Test) => { return testFunction(assert); });
+
+        }
+
+    }
+
     return tests;
 }
 
