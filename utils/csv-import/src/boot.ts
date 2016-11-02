5b445aee4de2debae8af01b693b233ce8469c5c4
Eusebio Olalde <eusebio.olalde@indexexchange.com>
2016-11-02 14:21:13 -0400

    [ATW-417] Added SchemaLoader and Validator to boot

--- utils/csv-import/src/boot.ts
+++ utils/csv-import/src/boot.ts
@@ -1,10 +1,17 @@
 'use strict';
 
 import { Injector } from './lib/injector';
-import { ConfigLoader, CsvLoader } from './lib/loaders';
+import { Validator } from './lib/validator';
+import { ConfigLoader, CsvLoader, SchemaLoader } from './lib/loaders';
 
 const configLoader = new ConfigLoader();
-Injector.put(configLoader, "ConfigLoader");
+Injector.put(configLoader, 'ConfigLoader');
 
 const csvLoader = new CsvLoader(configLoader);
-Injector.put(csvLoader, "CsvLoader");
+Injector.put(csvLoader, 'CsvLoader');
+
+const schemaLoader = new SchemaLoader();
+Injector.put(schemaLoader, 'SchemaLoader');
+
+const validator = new Validator(configLoader, schemaLoader);
+Injector.put(validator, 'Validator');
