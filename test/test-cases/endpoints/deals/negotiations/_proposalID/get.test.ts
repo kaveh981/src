749b2df91a5b9d989cfbae0e40a0f47dd4fc9c75
Ali Bhojani <ali.bhojani@indexexchange.com>
2016-11-03 15:17:30 -0400

    [ATW-422] all test cases now passing :D

--- test/test-cases/endpoints/deals/negotiations/_proposalID/get.test.ts
+++ test/test-cases/endpoints/deals/negotiations/_proposalID/get.test.ts
@@ -41,8 +41,8 @@ export let ATW_DN_GET_AUTH = authenticationTest(routePrefix + '/1', 'get', commo
  * @case    - Proposal ID is provided and is a valid number 
  * @expect  - 200 - 1 Deal Negotiation Object returned 
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing 
- * @tags    - contact
+ * @status  - passing 
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_01 (assert: test.Test) {
@@ -176,8 +176,8 @@ export async function ATW_DNP_GET_05 (assert: test.Test) {
  * @case    - Another Proposal (proposal2) concerning the user exists
  * @expect  - 200 - only the proposal specified is shown
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing 
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_06 (assert: test.Test) {
@@ -208,8 +208,8 @@ export async function ATW_DNP_GET_06 (assert: test.Test) {
  * @case    - Multiple Deal Negotations with the same proposal exist concerning different buyers 
  * @expect  - 200 - only the user's Deal Negotiations are shown
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_07 (assert: test.Test) {
@@ -238,8 +238,8 @@ export async function ATW_DNP_GET_07 (assert: test.Test) {
  * @case    - Multiple Deal Negotations with the same proposal concerning different publishers  
  * @expect  - 200 - only the user's Deal Negotiations are shown
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - pub, perspective
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_08 (assert: test.Test) {
@@ -295,8 +295,8 @@ export async function ATW_DNP_GET_09 (assert: test.Test) {
  * @case    - Publisher is not related to Deal Negotiations concerning Proposal specified  
  * @expect  - 200_NO_NEGOTIATIONS 
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing (not supported)
- * @tags    - pub, perspective
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_10 (assert: test.Test) {
@@ -346,8 +346,8 @@ export async function ATW_DNP_GET_11 (assert: test.Test) {
  * @case    - Publisher has deleted the deal negotiation 
  * @expect  - 200 - 1 Deal negotiation returned anyway 
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    -
  */
 
 export async function ATW_DNP_GET_12 (assert: test.Test) {
@@ -375,8 +375,8 @@ export async function ATW_DNP_GET_12 (assert: test.Test) {
  * @case    - Publisher has rejected the deal negotiation 
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_13 (assert: test.Test) {
@@ -404,8 +404,8 @@ export async function ATW_DNP_GET_13 (assert: test.Test) {
  * @case    - Publisher has accepted the deal negotiation 
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_14 (assert: test.Test) {
@@ -433,8 +433,8 @@ export async function ATW_DNP_GET_14 (assert: test.Test) {
  * @case    - Buyer has deleted the deal negotiation 
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_15 (assert: test.Test) {
@@ -462,8 +462,8 @@ export async function ATW_DNP_GET_15 (assert: test.Test) {
  * @case    - Buyer has rejected the deal negotiation 
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_16 (assert: test.Test) {
@@ -491,8 +491,8 @@ export async function ATW_DNP_GET_16 (assert: test.Test) {
  * @case    - Buyer has accepted the deal negotiation 
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_17 (assert: test.Test) {
@@ -520,8 +520,8 @@ export async function ATW_DNP_GET_17 (assert: test.Test) {
  * @case    - Deal Negotiation has not started, datewise 
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_18 (assert: test.Test) {
@@ -550,8 +550,8 @@ export async function ATW_DNP_GET_18 (assert: test.Test) {
  * @case    - Deal Negotiation has ended (datewise) 
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_19 (assert: test.Test) {
@@ -580,8 +580,8 @@ export async function ATW_DNP_GET_19 (assert: test.Test) {
  * @case    - Proposal is paused (status)
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_20 (assert: test.Test) {
@@ -609,8 +609,8 @@ export async function ATW_DNP_GET_20 (assert: test.Test) {
  * @case    - Proposal has been deleted (status)
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_21 (assert: test.Test) {
@@ -638,8 +638,8 @@ export async function ATW_DNP_GET_21 (assert: test.Test) {
  * @case    - Proposal owner is inactive
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_22 (assert: test.Test) {
@@ -665,8 +665,8 @@ export async function ATW_DNP_GET_22 (assert: test.Test) {
  * @case    - Proposal has expired (datewise)
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_23 (assert: test.Test) {
@@ -695,8 +695,8 @@ export async function ATW_DNP_GET_23 (assert: test.Test) {
  * @case    - Proposal has not yet started (datewise)
  * @expect  - 200 - 1 Deal negotiation returned anyway
  * @route   - GET deals/negotiations/proposal.proposal.proposalID
- * @status  - failing
- * @tags    - contact
+ * @status  - passing
+ * @tags    - 
  */
 
 export async function ATW_DNP_GET_24 (assert: test.Test) {
