3b7efd8363f7844bb0e39e5703e73a1a565bf1f7
Kaveh Fereidouni <kaveh.fereidouni@indexexchange.com>
2016-11-04 16:19:14 -0400

    [ATW-463] update payLoad

--- test/src/lib/helper.ts
+++ test/src/lib/helper.ts
@@ -26,61 +26,70 @@ class Helper {
      */
     public static proposalToPayload(proposal: INewProposalData, publisher: INewPubData) {
         return {
-            id: proposal.proposal.proposalID,
-            publisher_id: proposal.proposal.ownerID,
+            auction_type: proposal.proposal.auctionType,
+            budget: proposal.proposal.budget,
             contact: {
                 title: 'Warlord',
                 name: publisher.user.firstName + ' ' + publisher.user.lastName,
-                email_address: publisher.user.emailAddress,
+                email: publisher.user.emailAddress,
                 phone: publisher.user.phone
             },
-            name: proposal.proposal.name,
-            status: proposal.proposal.status,
+            created_at: proposal.proposal.createDate.toISOString(),
             currency: 'USD',
             description: proposal.proposal.description,
-            start_date: this.formatDate(proposal.proposal.startDate.toISOString()),
             end_date: this.formatDate(proposal.proposal.endDate.toISOString()),
-            price: proposal.proposal.price,
+            id: proposal.proposal.proposalID,
             impressions: proposal.proposal.impressions,
-            budget: proposal.proposal.budget,
-            auction_type: proposal.proposal.auctionType,
-            terms: proposal.proposal.terms,
-            created_at: proposal.proposal.createDate.toISOString(),
+            inventory: proposal.sectionIDs,
             modified_at: proposal.proposal.modifyDate.toISOString(),
-            deal_section_id: proposal.sectionIDs
+            name: proposal.proposal.name,
+            price: proposal.proposal.price,
+            owner_id: proposal.proposal.ownerID,
+            start_date: this.formatDate(proposal.proposal.startDate.toISOString()),
+            status: proposal.proposal.status,
+            terms: proposal.proposal.terms,
         };
     }
 
-    public static dealNegotiationToPayload (dealNegotiation: IDealNegotiationData, proposal: INewProposalData,
-                                            publisher: INewPubData, buyer: INewBuyerData) {
+    public static dealNegotiationToPayload(dealNegotiation: IDealNegotiationData, proposal: INewProposalData,
+        publisher: INewPubData, buyer: INewBuyerData) {
+        let partner = {};
+        if (dealNegotiation.sender === 'buyer') {
+            partner = {
+                id: buyer.user.userID,
+                contact: {
+                    title: 'Warlord',
+                    name: buyer.user.firstName + ' ' + buyer.user.lastName,
+                    email: buyer.user.emailAddress,
+                    phone: buyer.user.phone
+                }
+            };
+        } else {
+            partner = {
+                id: publisher.user.userID,
+                contact: {
+                    title: 'Warlord',
+                    name: publisher.user.firstName + ' ' + publisher.user.lastName,
+                    email: publisher.user.emailAddress,
+                    phone: publisher.user.phone
+                }
+            };
+        }
 
         return {
-            proposal_id: proposal.proposal.proposalID,
-            publisher_id: publisher.user.userID,
-            publisher_contact: {
-                title: 'Warlord',
-                name: publisher.user.firstName + ' ' + publisher.user.lastName,
-                email_address: publisher.user.emailAddress,
-                phone: publisher.user.phone
-            },
-            buyer_id: buyer.user.userID,
-            buyer_contact: {
-                title: 'Warlord',
-                name: buyer.user.firstName + ' ' + buyer.user.lastName,
-                email_address: buyer.user.emailAddress,
-                phone: buyer.user.phone
-            },
-            description: proposal.proposal.description,
-            terms: dealNegotiation.terms,
-            impressions: dealNegotiation.impressions,
-            budget: dealNegotiation.budget,
+            id: proposal.proposal.proposalID,
             name: proposal.proposal.name,
+            description: proposal.proposal.description,
+            auction_type: proposal.proposal.auctionType,
+            inventory: proposal.sectionIDs,
+            currency: 'USD',
             start_date: Helper.formatDate(dealNegotiation.startDate),
             end_date: Helper.formatDate(dealNegotiation.endDate),
-            auction_type: proposal.proposal.auctionType,
             price: dealNegotiation.price,
-            deal_section_id: proposal.sectionIDs,
-            currency: 'USD',
+            impressions: dealNegotiation.impressions,
+            budget: dealNegotiation.budget,
+            partner: partner,
+            terms: dealNegotiation.terms,
             created_at: dealNegotiation.createDate.toISOString(),
             modified_at: dealNegotiation.modifyDate.toISOString()
         };
