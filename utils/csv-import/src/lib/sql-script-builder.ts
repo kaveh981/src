a15497af561d74795cf38505c5b8d714173740cf
haiyang.sun <haiyang.sun@indexexchange.com>
2016-11-09 16:01:58 -0500

    [ATW-418] Add comments

--- utils/csv-import/src/lib/sql-script-builder.ts
+++ utils/csv-import/src/lib/sql-script-builder.ts
@@ -13,6 +13,9 @@ class SQLScriptBuilder {
         this.createDate = new Date();
     }
 
+    /**
+     * Build rollout and rollback scripts
+     */
     public async buildScripts(ticketNumber: string, proposals: INewProposalData[]) {
 
         let insertScript = await this.buildInsertScript(ticketNumber, proposals);
@@ -22,17 +25,20 @@ class SQLScriptBuilder {
         this.writeToFile(ticketNumber + "_rollback.sql", deleteScript);
     }
 
+    /**
+     * Build rollout script to insert proposals and related mappings
+     */
     public async buildInsertScript(ticketNumber: string, proposals: INewProposalData[]): Promise<string> {
 
         let insertScript: string;
-        insertScript = "/* JIRA Ticket Number: " + ticketNumber + " Date of Execution: " + this.createDate + " */\n";
 
-        insertScript += this.getExpectedChanges(proposals);
+        insertScript = "/* JIRA Ticket Number: " + ticketNumber + " Date of Execution: " + this.createDate + " */\n";
 
         insertScript += "TEE /tmp/" + ticketNumber + "_Viper2_rollout.log\n";
 
-        insertScript += "SET @existing_proposals = (SELECT COUNT(*) FROM ixmDealProposals);\n";
+        insertScript += this.getExpectedChanges(proposals);
 
+        insertScript += "SET @existing_proposals = (SELECT COUNT(*) FROM ixmDealProposals);\n";
         insertScript += "SET @existing_mappings = (SELECT COUNT(*) FROM ixmProposalSectionMappings);\n";
 
         insertScript += "START TRANSACTION;";
@@ -50,16 +56,20 @@ class SQLScriptBuilder {
         return insertScript;
     }
 
+    /**
+     * Build rollback script to delete proposals and related mappings
+     */
     public async buildDeleteScript(ticketNumber: string, proposals: INewProposalData[]) {
-        let deleteScript: string;
-        deleteScript = "/* JIRA Ticket Number: " + ticketNumber + " Date of Execution: " + this.createDate + " */\n";
 
-        deleteScript += this.getExpectedChanges(proposals);
+        let deleteScript: string;
+
+        deleteScript = "/* JIRA Ticket Number: " + ticketNumber + " Date of Execution: " + this.createDate + " */\n";
 
         deleteScript += "TEE /tmp/" + ticketNumber + "_Viper2_rollback.log\n";
 
-        deleteScript += "SET @existing_proposals = (SELECT COUNT(*) FROM ixmDealProposals);\n";
+        deleteScript += this.getExpectedChanges(proposals);
 
+        deleteScript += "SET @existing_proposals = (SELECT COUNT(*) FROM ixmDealProposals);\n";
         deleteScript += "SET @existing_mappings = (SELECT COUNT(*) FROM ixmProposalSectionMappings);\n";
 
         deleteScript += "START TRANSACTION;";
@@ -76,6 +86,9 @@ class SQLScriptBuilder {
         return deleteScript;
     }
 
+    /**
+     * Build query to insert a proposal and related mappings
+     */
     private async buildInsertQuery(proposal: IProposal, sectionIDs: number[]): Promise<string> {
 
         let query: string;
@@ -92,6 +105,9 @@ class SQLScriptBuilder {
         return query;
     }
 
+    /**
+     * Build query to delete a proposal and related mappings
+     */
     private async buildDeleteQuery(proposal: IProposal, sectionIDs: number[]): Promise<string> {
         let query: string;
 
@@ -110,6 +126,10 @@ class SQLScriptBuilder {
 
     }
 
+    /**
+     * Get string of SQL commands that store changes of ixmDealProposals table and 
+     * ixmProposalSectionMappings table in variables
+     */
     public getExpectedChanges(proposals: INewProposalData[]) {
         let changes: string = "SET @expected_proposal_changes = " + proposals.length + ";\n" ;
 
@@ -124,6 +144,9 @@ class SQLScriptBuilder {
         return changes;
     }
 
+    /**
+     * Write string to file
+     */
     private writeToFile(path: string, content: string) {
         let fs = require('fs');
         fs.writeFile(path, content, function(err) {
