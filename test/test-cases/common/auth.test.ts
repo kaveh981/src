5a6e6d6cd9d61847c413586f44476858244c8723
haiyang.sun <haiyang.sun@indexexchange.com>
2016-11-10 17:51:09 -0500

    [ATW-360] Update Auth test to support publisher

--- test/test-cases/common/auth.test.ts
+++ test/test-cases/common/auth.test.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import * as test from 'tape';
+import * as request from 'request';
 
 import { Injector } from '../../src/lib/injector';
 import { APIRequestManager } from '../../src/lib/request-manager';
@@ -10,7 +11,7 @@ const databasePopulator = Injector.request<DatabasePopulator>('DatabasePopulator
 const apiRequest = Injector.request<APIRequestManager>('APIRequestManager');
 
  /*
- * @case    - The buyer is an IXM Buyer.
+ * @case    - The user is an IXM User (IXM Buyer or IXM Publisher.
  * @expect  - The response has status code which isn't 500 or 401.
  * @status  - working
  * @tags    - get, auth, buyer
@@ -18,23 +19,28 @@ const apiRequest = Injector.request<APIRequestManager>('APIRequestManager');
 async function ATW_AUTH_01 (route: string, verb: string, setup: Function, assert: test.Test) {
 
     /** Setup */
-    assert.plan(2);
+    assert.plan(4);
 
     await setup();
 
     let dsp = await databasePopulator.createDSP(1);
     let buyer = await databasePopulator.createBuyer(dsp.dspID);
+    let publisher = await databasePopulator.createPublisher();
 
     /** Test */
-    let response = await apiRequest[verb](route, {}, buyer.user.userID);
+    let buyerResponse = await apiRequest[verb](route, {}, buyer.user.userID);
 
-    assert.not(response.status, 401);
-    assert.not(response.status, 500);
+    assert.not(buyerResponse.status, 401);
+    assert.not(buyerResponse.status, 500);
 
+    let pubResponse = await apiRequest[verb](route, {}, publisher.user.userID);
+
+    assert.not(buyerResponse.status, 401);
+    assert.not(buyerResponse.status, 500);
 }
 
  /*
- * @case    - The user is not an IXM Buyer.
+ * @case    - The user is not an IXM User.
  * @expect  - The response has status code 401.
  * @status  - working
  * @tags    - get,auth,buyer
@@ -56,7 +62,7 @@ async function ATW_AUTH_02 (route: string, verb: string, setup: Function, assert
 }
 
  /*
- * @case    - The buyer id is not an integer.
+ * @case    - The user id is not an valid (String, Float, Negative, Boolean, Array, Object, Function).
  * @expect  - The response has status code 401.
  * @status  - working
  * @tags    - get, auth, buyer
@@ -64,19 +70,35 @@ async function ATW_AUTH_02 (route: string, verb: string, setup: Function, assert
 async function ATW_AUTH_03 (route: string, verb: string, setup: Function, assert: test.Test) {
 
     /** Setup */
-    assert.plan(1);
+    assert.plan(7);
 
     await setup();
 
     /** Test */
     let response = await apiRequest[verb](route, {}, 'goose bear');
-
     assert.equal(response.status, 401);
 
+    response = await apiRequest[verb](route, {}, 3.1415926 );
+    assert.equal(response.status, 401);
+
+    response = await apiRequest[verb](route, {}, -1 );
+    assert.equal(response.status, 401);
+
+    response = await apiRequest[verb](route, {}, true );
+    assert.equal(response.status, 401);
+
+    response = await apiRequest[verb](route, {}, [3, 1, 4] );
+    assert.equal(response.status, 401);
+
+    response = await apiRequest[verb](route, {}, {goose: 314} );
+    assert.equal(response.status, 401);
+
+    response = await apiRequest[verb](route, {}, function() {return; } );
+    assert.equal(response.status, 401);
 }
 
  /*
- * @case    - The buyer id is not supplied.
+ * @case    - The user id is not supplied.
  * @expect  - The response has status code 401.
  * @status  - working
  * @tags    - get, auth, buyer
@@ -92,7 +114,6 @@ async function ATW_AUTH_04 (route: string, verb: string, setup: Function, assert
     let response = await apiRequest[verb](route, {});
 
     assert.equal(response.status, 401);
-
 }
 
 /**
