ecd1bcf8285d233207bea74a39d9c1e016de5008
Nicholas Leavitt <nicholas.leavitt@indexexchange.com>
2016-11-11 09:55:03 -0500

    [ATW-476] Made ids naming consistent across objects, as well as the proposal object.

--- api/src/models/deals/settled-deal/settled-deal-model.ts
+++ api/src/models/deals/settled-deal/settled-deal-model.ts
@@ -68,54 +68,27 @@ class SettledDealModel {
         let partner;
 
         if (userType === 'IXMB') {
-            partner = { id: this.negotiatedDeal.publisherID, contact: this.negotiatedDeal.publisherInfo.toContactPayload() };
+            partner = this.negotiatedDeal.publisherInfo.toPayload('partner_id');
         } else {
-            partner = { id: this.negotiatedDeal.buyerID, contact: this.negotiatedDeal.buyerInfo.toContactPayload() };
+            partner = this.negotiatedDeal.buyerInfo.toPayload('partner_id');
         }
 
-        if (this.isIXMDeal) {
-            // IXM deals have more information
-            return {
-                proposal: {
-                    id: this.negotiatedDeal.proposedDeal.id,
-                    name: this.negotiatedDeal.proposedDeal.name,
-                    description: this.negotiatedDeal.proposedDeal.description,
-                },
-                partner: partner,
-                dsp_id: this.dspID,
-                terms: this.negotiatedDeal.terms,
-                impressions: this.negotiatedDeal.impressions,
-                budget: this.negotiatedDeal.budget,
-                auction_type: this.negotiatedDeal.proposedDeal.auctionType,
-                inventory: this.negotiatedDeal.proposedDeal.sections,
-                currency: this.negotiatedDeal.proposedDeal.currency,
-                external_id: this.externalDealID,
-                start_date: Helper.formatDate(this.startDate),
-                end_date: Helper.formatDate(this.endDate),
-                status: this.status,
-                priority: this.priority,
-                price: this.negotiatedDeal.price,
-                created_at: this.createDate.toISOString(),
-                modified_at: this.modifyDate.toISOString()
-            };
-        } else {
-            // Non-ixm deals have less information
-            return {
-                dsp_id: this.dspID,
-                status: this.status,
-                external_id: this.externalDealID,
-
-                partner: partner,
-                price: this.negotiatedDeal.price,
-                start_date: this.negotiatedDeal.startDate,
-                end_date: this.negotiatedDeal.endDate,
-
-                auction_type: this.negotiatedDeal.proposedDeal.auctionType,
-                inventory: this.negotiatedDeal.proposedDeal.sections,
-                currency: this.negotiatedDeal.proposedDeal.currency,
-                name: this.negotiatedDeal.proposedDeal.name
-            };
-        }
+        return {
+            proposal: this.negotiatedDeal.proposedDeal.toSubPayload(),
+            partner: partner,
+            dsp_id: this.dspID,
+            terms: this.negotiatedDeal.terms,
+            impressions: this.negotiatedDeal.impressions,
+            budget: this.negotiatedDeal.budget,
+            external_id: this.externalDealID,
+            start_date: Helper.formatDate(this.startDate),
+            end_date: Helper.formatDate(this.endDate),
+            status: this.status,
+            priority: this.priority,
+            price: this.negotiatedDeal.price,
+            created_at: this.createDate.toISOString(),
+            modified_at: this.modifyDate.toISOString()
+        };
 
     }
 
