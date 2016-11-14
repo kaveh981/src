6147755fc5f57429d5e2080e29f03d123fecf8f4
Ali Bhojani <ali.bhojani@indexexchange.com>
2016-11-14 17:28:31 -0500

    [ATW-464] converter.ts

---
+++ test/test-cases/stress-tests/converter.ts
@@ -0,0 +1,14 @@
+'use strict'; 
+
+module.exports = {
+  stringToNumber: stringToNumber,
+}
+
+function stringToNumber(requestParams, context, ee, next) {
+
+    requestParams.json['proposal_id'] = Number(requestParams.json['proposal_id']);
+    requestParams.json['partner_id'] = Number(requestParams.json['partner_id']);   
+    return next(); 
+}
+
+
