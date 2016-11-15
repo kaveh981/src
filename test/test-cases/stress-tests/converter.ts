5640419df1d8139ec1e1dc8f23878bba4bbc7dd8
Ali Bhojani <ali.bhojani@indexexchange.com>
2016-11-15 17:12:53 -0500

    [ATW-464] made the code badsyled

--- test/test-cases/stress-tests/converter.ts
+++ test/test-cases/stress-tests/converter.ts
@@ -1,9 +1,11 @@
 'use strict';
 
-exports.stringToNumber = function (requestParams, context, ee, next) {
+function stringToNumber (requestParams, context, ee, next) {
 
     requestParams.json['proposal_id'] = Number(requestParams.json['proposal_id']);
     requestParams.json['partner_id'] = Number(requestParams.json['partner_id']);
     return next();
 
 };
+
+module.exports = stringToNumber;
