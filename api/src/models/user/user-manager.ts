2b8e6454103018532d517aa2b0ffaadf1ff192f9
Nicholas Leavitt <nicholas.leavitt@indexexchange.com>
2016-11-21 16:18:16 -0500

    [ATW-123] Undid an unintentional change.

--- api/src/models/user/user-manager.ts
+++ api/src/models/user/user-manager.ts
@@ -24,7 +24,7 @@ class UserManager {
      * @param userId - The id of the user we want information from.
      * @returns A user model for that user.
      */
-    public async fetchUserFromId(userID: any): Promise<UserModel> {
+    public async fetchUserFromId(userID: number): Promise<UserModel> {
 
         let rows = await this.databaseManager.select('userID as id', 'status', 'userTypes.name as userType',
                                                      'ug.name as userGroup', 'firstName', 'lastName', 'emailAddress', 'phone')
