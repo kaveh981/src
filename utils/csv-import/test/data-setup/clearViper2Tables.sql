abbf2c5786fc5b2db3609905a46299b143b8b204
Eusebio Olalde <eusebio.olalde@indexexchange.com>
2016-12-07 15:45:20 -0500

    [ATW-594] Added backup & restore for rtbDSPs

--- utils/csv-import/test/data-setup/clearViper2Tables.sql
+++ utils/csv-import/test/data-setup/clearViper2Tables.sql
@@ -4,22 +4,25 @@ SET foreign_key_checks=0;
 
 DROP TABLE IF EXISTS `users_backup`;
 DROP TABLE IF EXISTS `rtbSections_backup`;
+DROP TABLE IF EXISTS `rtbDSPs_backup`;
 DROP TABLE IF EXISTS `ixmDealProposals_backup`;
 DROP TABLE IF EXISTS `ixmBuyers_backup`;
 DROP TABLE IF EXISTS `ixmProposalTargeting_backup`;
 DROP TABLE IF EXISTS `ixmProposalSectionMappings_backup`;
 
-CREATE TABLE IF NOT EXISTS `users_backup` SELECT *  FROM `users`;
-CREATE TABLE IF NOT EXISTS `rtbSections_backup` SELECT *  FROM `rtbSections`;
-CREATE TABLE IF NOT EXISTS `ixmDealProposals_backup` SELECT *  FROM `ixmDealProposals`;
-CREATE TABLE IF NOT EXISTS `ixmBuyers_backup` SELECT *  FROM `ixmBuyers`;
-CREATE TABLE IF NOT EXISTS `ixmProposalTargeting_backup` SELECT *  FROM `ixmProposalTargeting`;
-CREATE TABLE IF NOT EXISTS `ixmProposalSectionMappings_backup` SELECT *  FROM `ixmProposalSectionMappings`;
+CREATE TABLE IF NOT EXISTS `users_backup` SELECT * FROM `users`;
+CREATE TABLE IF NOT EXISTS `rtbSections_backup` SELECT * FROM `rtbSections`;
+CREATE TABLE IF NOT EXISTS `rtbDSPs_backup` SELECT * FROM `rtbDSPs`;
+CREATE TABLE IF NOT EXISTS `ixmDealProposals_backup` SELECT * FROM `ixmDealProposals`;
+CREATE TABLE IF NOT EXISTS `ixmBuyers_backup` SELECT * FROM `ixmBuyers`;
+CREATE TABLE IF NOT EXISTS `ixmProposalTargeting_backup` SELECT * FROM `ixmProposalTargeting`;
+CREATE TABLE IF NOT EXISTS `ixmProposalSectionMappings_backup` SELECT * FROM `ixmProposalSectionMappings`;
 
 -- CLEAR TABLES
 
 TRUNCATE `users`;
 TRUNCATE `rtbSections`;
+TRUNCATE `rtbDSPs`;
 TRUNCATE `ixmDealProposals`;
 TRUNCATE `ixmBuyers`;
 TRUNCATE `ixmProposalTargeting`;
--- utils/csv-import/test/data-setup/restoreViper2Tables.sql
+++ utils/csv-import/test/data-setup/restoreViper2Tables.sql
@@ -4,6 +4,7 @@ SET foreign_key_checks=0;
 
 TRUNCATE `users`;
 TRUNCATE `rtbSections`;
+TRUNCATE `rtbDSPs`;
 TRUNCATE `ixmDealProposals`;
 TRUNCATE `ixmBuyers`;
 TRUNCATE `ixmProposalTargeting`;
@@ -13,6 +14,7 @@ TRUNCATE `ixmProposalSectionMappings`;
 
 INSERT INTO `users` SELECT * FROM `users_backup`;
 INSERT INTO `rtbSections` SELECT * FROM `rtbSections_backup`;
+INSERT INTO `rtbDSPs` SELECT * FROM `rtbDSPs_backup`;
 INSERT INTO `ixmDealProposals` SELECT * FROM `ixmDealProposals_backup`;
 INSERT INTO `ixmBuyers` SELECT * FROM `ixmBuyers_backup`;
 INSERT INTO `ixmProposalTargeting` SELECT * FROM `ixmProposalTargeting_backup`;
@@ -22,6 +24,7 @@ INSERT INTO `ixmProposalSectionMappings` SELECT * FROM `ixmProposalSectionMappin
 
 DROP TABLE `users_backup`;
 DROP TABLE `rtbSections_backup`;
+DROP TABLE `rtbDSPs_backup`;
 DROP TABLE `ixmDealProposals_backup`;
 DROP TABLE `ixmBuyers_backup`;
 DROP TABLE `ixmProposalTargeting_backup`;
