6531c4a1fa13b7b089b3473d7f80cf0f9cc51a85
haiyang.sun <haiyang.sun@indexexchange.com>
2016-12-19 14:38:27 -0500

    [ATW-389] Get legacy deal route

--- api/src/controllers/deals/active/index.ts
+++ api/src/controllers/deals/active/index.ts
@@ -1,12 +1,15 @@
 'use strict';
 
 import * as express from 'express';
+import * as request from 'request-promise-native'
 
 import { Logger } from '../../../lib/logger';
 import { Injector } from '../../../lib/injector';
 import { RamlTypeValidator } from '../../../lib/raml-type-validator';
 import { HTTPError } from '../../../lib/http-error';
 import { ProtectedRoute } from '../../../middleware/protected-route';
+import { ConfigLoader } from '../../../lib/config-loader';
+
 
 import { SettledDealManager } from '../../../models/deals/settled-deal/settled-deal-manager';
 import { SettledDealModel } from '../../../models/deals/settled-deal/settled-deal-model';
@@ -26,6 +29,9 @@ const databaseManager = Injector.request<DatabaseManager>('DatabaseManager');
 const buyerManager = Injector.request<BuyerManager>('BuyerManager');
 const publisherManager = Injector.request<PublisherManager>('PublisherManager');
 const validator = Injector.request<RamlTypeValidator>('Validator');
+const config = Injector.request<ConfigLoader>('ConfigLoader');
+ 
+const authConfig = config.get('auth');
 
 const Log: Logger = new Logger('ROUT');
 
@@ -35,7 +41,7 @@ const Log: Logger = new Logger('ROUT');
 function ActiveDeals(router: express.Router): void {
 
     /**
-     * GET request to get all active deals.
+     * GET request to get all active deals created from Index Market.
      */
     router.get('/', ProtectedRoute, async (req: express.Request, res: express.Response, next: Function) => { try {
 
@@ -59,6 +65,34 @@ function ActiveDeals(router: express.Router): void {
         Log.trace(`Found deals ${Log.stringify(activeDeals)} for user ${user.id}.`, req.id);
 
         res.sendPayload(activeDeals.map((deal) => { return deal.toPayload(req.ixmUserInfo.userType); }), pagination.toPayload());
+
+    } catch (error) { next(error); } });
+    
+    /**
+      * GET request to get all active and paused deals.
+      */
+    router.get('/legacy', ProtectedRoute, async (req: express.Request, res: express.Response, next: Function) => { try {
+
+        Log.trace(`Get deals request received, retrieve from Index Exchange API`, req.id);
+
+        let accessToken = req.get(authConfig['tokenHeader']);
+
+        let options = {
+            url: `https://api01.indexexchange.com/api/publishers/deals?userID=${req.ixmUserInfo.id}`,
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            json: true,
+            rejectUnauthorized: false
+        };
+
+        let response = await request(options).catch((err) => {
+            if (err.name === 'StatusCodeError') {
+                return err.error;
+            }
+        });
+
+        res.status(response.responseCode).send(response);
 
     } catch (error) { next(error); } });
 
