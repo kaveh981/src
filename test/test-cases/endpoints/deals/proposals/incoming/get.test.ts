471410b9034c3b8e174fe499e3d62d7cd3744644
Johnny El-Hayek <johnny.elhayek@indexexchange.com>
2017-01-25 15:08:16 -0500

    [ATW-752] Updated incoming and list proposals

--- test/test-cases/endpoints/deals/proposals/incoming/get.test.ts
+++ test/test-cases/endpoints/deals/proposals/incoming/get.test.ts
@@ -202,7 +202,7 @@ export async function ATW_API_GET_DEAPROINC_FUNC_04 (assert: test.Test) {
     let proposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ], {}, [ pubCompany.user.userID ]);
 
     // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
-    let openProposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ]);
+    let openProposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ]);
 
     /** 
      * Test 
@@ -243,7 +243,7 @@ export async function ATW_API_GET_DEAPROINC_FUNC_05 (assert: test.Test) {
     let proposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ], {}, [ pubCompany2.user.userID ]);
 
     // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
-    let openProposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ]);
+    let openProposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ]);
 
     /** Test */
     let response = await apiRequest.get(route, {}, pubCompany.user);
@@ -356,7 +356,7 @@ export async function ATW_API_GET_DEAPROINC_FUNC_08 (assert: test.Test) {
                                                           { status: 'deleted' }, [ pubCompany.user.userID ]);
 
     // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
-    let openProposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ]);
+    let openProposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ]);
 
     /** Test */
     let response = await apiRequest.get(route, {}, pubCompany.user);
@@ -368,7 +368,7 @@ export async function ATW_API_GET_DEAPROINC_FUNC_08 (assert: test.Test) {
 
 /**
  * @case    - A publisher company views proposals targeted to them, and a buyer has targeted them in an expired proposal.
- * @expect  - The publisher should receive nothin.
+ * @expect  - The publisher should receive the defined proposal.
  * @route   - GET deals/proposals/incoming
  * @status  - working
  * @tags    - get, deals
@@ -391,14 +391,199 @@ export async function ATW_API_GET_DEAPROINC_FUNC_09 (assert: test.Test) {
 
     let proposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ],
                                                           { startDate: '1992-07-29', endDate: '2000-12-01' }, [ pubCompany.user.userID ]);
+    proposal.proposal.status = 'expired';
 
     // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
-    let openProposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ]);
+    let openProposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ]);
 
     /** Test */
     let response = await apiRequest.get(route, {}, pubCompany.user);
 
     assert.equals(response.status, 200);
-    assert.deepEquals(response.body['data'], []);
+    assert.deepEquals(response.body['data'], [ await Helper.proposalToPayload(proposal, buyerCompany.user) ]);
+
+}
+
+/**
+ * @case    - A buyer company views proposals targeted to them, and a publisher has targeted them in an expired proposal.
+ * @expect  - The buyer should receive the defined proposal.
+ * @route   - GET deals/proposals/incoming
+ * @status  - working
+ * @tags    - get, deals
+ */
+export async function ATW_API_GET_DEAPROINC_FUNC_10 (assert: test.Test) {
+
+    /** Setup */
+    assert.plan(2);
+
+    let dsp = await databasePopulator.createDSP(1);
+
+    let buyerCompany = await databasePopulator.createCompany({}, dsp.dspID);
+    let buyer = await databasePopulator.createBuyer(buyerCompany.user.userID, 'write');
+
+    let pubCompany = await databasePopulator.createCompany();
+    let publisher = await databasePopulator.createPublisher(pubCompany.user.userID, 'write');
+
+    let site = await databasePopulator.createSite(pubCompany.user.userID);
+    let section = await databasePopulator.createSection(pubCompany.user.userID, [ site.siteID ]);
+
+    let proposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ],
+                                                          { startDate: '1992-07-29', endDate: '2000-12-01' }, [ buyerCompany.user.userID ]);
+    proposal.proposal.status = 'expired';
+
+    // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
+    let openProposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ]);
+
+    /** Test */
+    let response = await apiRequest.get(route, {}, buyerCompany.user);
+
+    assert.equals(response.status, 200);
+    assert.deepEquals(response.body['data'], [ await Helper.proposalToPayload(proposal, pubCompany.user) ]);
+
+}
+
+/**
+ * @case    - A publisher company views proposals targeted to them, and a buyer has targeted them in a proposal with no sections.
+ * @expect  - The publisher should receive the defined proposal.
+ * @route   - GET deals/proposals/incoming
+ * @status  - working
+ * @tags    - get, deals
+ */
+export async function ATW_API_GET_DEAPROINC_FUNC_11 (assert: test.Test) {
+
+    /** Setup */
+    assert.plan(2);
+
+    let dsp = await databasePopulator.createDSP(1);
+
+    let buyerCompany = await databasePopulator.createCompany({}, dsp.dspID);
+    let buyer = await databasePopulator.createBuyer(buyerCompany.user.userID, 'write');
+
+    let pubCompany = await databasePopulator.createCompany();
+    let publisher = await databasePopulator.createPublisher(pubCompany.user.userID, 'write');
+
+    let site = await databasePopulator.createSite(pubCompany.user.userID);
+    let section = await databasePopulator.createSection(pubCompany.user.userID, [ site.siteID ]);
+
+    let proposal = await databasePopulator.createProposal(buyerCompany.user.userID, [], {}, [ pubCompany.user.userID ]);
+
+    // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
+    let openProposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ]);
+
+    /** Test */
+    let response = await apiRequest.get(route, {}, pubCompany.user);
+
+    assert.equals(response.status, 200);
+    assert.deepEquals(response.body['data'], [ await Helper.proposalToPayload(proposal, buyerCompany.user) ]);
+
+}
+
+/**
+ * @case    - A buyer company views proposals targeted to them, and a publisher has targeted them in a proposal with no sections.
+ * @expect  - The buyer should receive the defined proposal.
+ * @route   - GET deals/proposals/incoming
+ * @status  - working
+ * @tags    - get, deals
+ */
+export async function ATW_API_GET_DEAPROINC_FUNC_12 (assert: test.Test) {
+
+    /** Setup */
+    assert.plan(2);
+
+    let dsp = await databasePopulator.createDSP(1);
+
+    let buyerCompany = await databasePopulator.createCompany({}, dsp.dspID);
+    let buyer = await databasePopulator.createBuyer(buyerCompany.user.userID, 'write');
+
+    let pubCompany = await databasePopulator.createCompany();
+    let publisher = await databasePopulator.createPublisher(pubCompany.user.userID, 'write');
+
+    let site = await databasePopulator.createSite(pubCompany.user.userID);
+    let section = await databasePopulator.createSection(pubCompany.user.userID, [ site.siteID ]);
+
+    let proposal = await databasePopulator.createProposal(pubCompany.user.userID, [], {}, [ buyerCompany.user.userID ]);
+
+    // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
+    let openProposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ]);
+
+    /** Test */
+    let response = await apiRequest.get(route, {}, buyerCompany.user);
+
+    assert.equals(response.status, 200);
+    assert.deepEquals(response.body['data'], [ await Helper.proposalToPayload(proposal, pubCompany.user) ]);
+
+}
+
+/**
+ * @case    - A publisher company views proposals targeted to them, and a buyer has targeted them in a proposal with deleted sections.
+ * @expect  - The publisher should receive the defined proposal.
+ * @route   - GET deals/proposals/incoming
+ * @status  - working
+ * @tags    - get, deals
+ */
+export async function ATW_API_GET_DEAPROINC_FUNC_13 (assert: test.Test) {
+
+    /** Setup */
+    assert.plan(2);
+
+    let dsp = await databasePopulator.createDSP(1);
+
+    let buyerCompany = await databasePopulator.createCompany({}, dsp.dspID);
+    let buyer = await databasePopulator.createBuyer(buyerCompany.user.userID, 'write');
+
+    let pubCompany = await databasePopulator.createCompany();
+    let publisher = await databasePopulator.createPublisher(pubCompany.user.userID, 'write');
+
+    let site = await databasePopulator.createSite(pubCompany.user.userID);
+    let section = await databasePopulator.createSection(pubCompany.user.userID, [ site.siteID ], { status: 'D' });
+
+    let proposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ], {}, [ pubCompany.user.userID ]);
+    proposal.sectionIDs = [];
+
+    // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
+    let openProposal = await databasePopulator.createProposal(buyerCompany.user.userID, [ section.section.sectionID ]);
+
+    /** Test */
+    let response = await apiRequest.get(route, {}, pubCompany.user);
+
+    assert.equals(response.status, 200);
+    assert.deepEquals(response.body['data'], [ await Helper.proposalToPayload(proposal, buyerCompany.user) ]);
+
+}
+
+/**
+ * @case    - A buyer company views proposals targeted to them, and a publisher has targeted them in a proposal with deleted sections.
+ * @expect  - The buyer should receive the defined proposal.
+ * @route   - GET deals/proposals/incoming
+ * @status  - working
+ * @tags    - get, deals
+ */
+export async function ATW_API_GET_DEAPROINC_FUNC_14 (assert: test.Test) {
+
+    /** Setup */
+    assert.plan(2);
+
+    let dsp = await databasePopulator.createDSP(1);
+
+    let buyerCompany = await databasePopulator.createCompany({}, dsp.dspID);
+    let buyer = await databasePopulator.createBuyer(buyerCompany.user.userID, 'write');
+
+    let pubCompany = await databasePopulator.createCompany();
+    let publisher = await databasePopulator.createPublisher(pubCompany.user.userID, 'write');
+
+    let site = await databasePopulator.createSite(pubCompany.user.userID);
+    let section = await databasePopulator.createSection(pubCompany.user.userID, [ site.siteID ], { status: 'D' });
+
+    let proposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ], {}, [ buyerCompany.user.userID ]);
+    proposal.sectionIDs = [];
+
+    // Open proposal to make sure we aren't getting more than we want ie. only targeted proposals
+    let openProposal = await databasePopulator.createProposal(pubCompany.user.userID, [ section.section.sectionID ]);
+
+    /** Test */
+    let response = await apiRequest.get(route, {}, buyerCompany.user);
+
+    assert.equals(response.status, 200);
+    assert.deepEquals(response.body['data'], [ await Helper.proposalToPayload(proposal, pubCompany.user) ]);
 
 }
