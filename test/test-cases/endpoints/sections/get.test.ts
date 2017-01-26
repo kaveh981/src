89dff315c5fcb6ea2632a2a5b4e3391531269321
Johnny El-Hayek <johnny.elhayek@indexexchange.com>
2017-01-26 15:53:24 -0500

    [ATW-846] Fixed tslint issue

--- test/test-cases/endpoints/sections/get.test.ts
+++ test/test-cases/endpoints/sections/get.test.ts
@@ -5,12 +5,10 @@ import * as test from 'tape';
 import { Injector } from '../../../src/lib/injector';
 import { APIRequestManager } from '../../../src/lib/request-manager';
 import { DatabasePopulator } from '../../../src/lib/database-populator';
-import { DatabaseManager } from '../../../src/lib/database-manager';
 import { Helper } from '../../../src/lib/helper';
 
 const databasePopulator = Injector.request<DatabasePopulator>('DatabasePopulator');
 const apiRequest = Injector.request<APIRequestManager>('APIRequestManager');
-const databaseManager = Injector.request<DatabaseManager>('DatabaseManager');
 
 /** Test constants */
 const route = 'sections';
